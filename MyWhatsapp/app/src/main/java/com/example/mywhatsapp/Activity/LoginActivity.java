720f5c0ba9adc3c740db419a9ca92fbbdf8bf9d1
Shivam5075455 <sk5075455@gmail.com>
2024-04-25 14:04:35 +0530

    First commit

---
+++ MyWhatsapp/app/src/main/java/com/example/mywhatsapp/Activity/LoginActivity.java
@@ -0,0 +1,59 @@
+package com.example.mywhatsapp.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.mywhatsapp.MainActivity;
+import com.example.mywhatsapp.R;
+import com.example.mywhatsapp.databinding.ActivityLoginBinding;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
+    FirebaseAuth auth;
+
+    final static String TAG = "mydebug";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        auth = FirebaseAuth.getInstance();
+        binding.btnLogin.setOnClickListener(v ->
+                logIn(binding.etEmail.getText().toString(), binding.etPassword.getText().toString()));
+    }
+
+    public void logIn(String email, String password) {
+        if (!binding.etEmail.getText().toString().isEmpty() && !binding.etPassword.getText().toString().isEmpty()) {
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                if (task.isSuccessful()){
+                    startActivity(new Intent(this, MainActivity.class));
+                    Log.d(TAG,"Successfully login");
+                    finish();
+                }else{
+                    Toast.makeText(this, "Error: "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    Log.d(TAG,"Login failed");
+                }
+            });
+        }else{
+            Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
+        }
+    }//log in
+}
---
+++ MyWhatsapp/app/src/main/java/com/example/mywhatsapp/Activity/SignUpActivity.java
@@ -0,0 +1,87 @@
+package com.example.mywhatsapp.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.mywhatsapp.R;
+import com.example.mywhatsapp.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    ActivitySignUpBinding binding;
+
+    final static String TAG = "mydebug";
+
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        auth = FirebaseAuth.getInstance();
+        binding.btnSignup.setOnClickListener(v ->signupwithEmail(binding.etEmail.getText().toString(), binding.etPassword.getText().toString()));
+
+
+    }
+
+    public void signupwithEmail(String email, String password){
+
+        if(!binding.etName.getText().toString().isEmpty() && !binding.etEmail.getText().toString().isEmpty() && !binding.etPassword.getText().toString().isEmpty()){
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                if (task.isSuccessful()){
+                    String uid = Objects.requireNonNull(auth.getCurrentUser()).getUid();
+                    DatabaseReference databaseReference = FirebaseDatabase.getInstance("https://mywhatsapp-d2663-default-rtdb.firebaseio.com/").getReference("users").child(uid);
+                    HashMap<String, Object> hashMap = new HashMap<>();
+                    hashMap.put("uid", uid);
+                    hashMap.put("name", binding.etName.getText().toString());
+                    hashMap.put("email",binding.etEmail.getText().toString());
+                    hashMap.put("password",binding.etPassword.getText().toString());
+                    hashMap.put("image","");
+                    databaseReference.setValue(hashMap).addOnCompleteListener(task1 -> {
+                        if (task1.isSuccessful()){
+                            Toast.makeText(this, "Account created successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+                            finish();
+                            Log.d(TAG,"Account created and stored in fireabsde");
+                        }else{
+                            Toast.makeText(this, "Somethind went wrong", Toast.LENGTH_SHORT).show();
+                            Log.d(TAG,"somethind went wrong");
+                        }
+                    });
+                }else {
+                    Toast.makeText(this, "This account is already exist", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG,"This account is already exist");
+                }
+            });
+        }else{
+            Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
+
+
+        }
+    }
+}
